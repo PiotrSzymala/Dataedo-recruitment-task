663d201b55bf54e497eaca4c2ed4d8e932100780
Piotr Szymala <p.szymala@sellpander.pl>
2024-06-06 14:24:37 +0200

    Dodaj pliki projektów.

---
+++ Dataedo-recruitment-task/Controllers/DateoApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dataedo_recruitment_task.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DateoApiController : ControllerBase
+    {
+    }
+}
