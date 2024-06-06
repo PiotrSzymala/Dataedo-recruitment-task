78c7eec4419e0e7d629c35a0a99f798ee88dc1fd
Piotr Szymala <p.szymala@sellpander.pl>
2024-06-06 14:25:32 +0200

    feature: add endpoint to delete user by id.

--- Dataedo-recruitment-task/Controllers/DateoApiController.cs
+++ Dataedo-recruitment-task/Controllers/DateoApiController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Dataedo_recruitment_task.Controllers
 {
@@ -6,5 +7,14 @@ namespace Dataedo_recruitment_task.Controllers
     [Route("[controller]")]
     public class DateoApiController : ControllerBase
     {
+        [HttpPost("delete/{id}")]
+        public void Delete(uint id)
+        {
+            User user = _context.Users.FirstOrDefault(user => user.Id == id);
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+            Debug.WriteLine($"The user with Login={user.login} has been deleted.");
+            return Ok();
+        }
     }
 }
